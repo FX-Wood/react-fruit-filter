76e6e3d26b80d256fcba8ebd2f937cb8a007fd27
FX-Wood <fxwood2@gmail.com>
2019-03-14 10:43:56 -0700

    Adding a list of fruits

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,60 @@ import './index.css';
 import FruitFilter from './FruitFilter';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<FruitFilter />, document.getElementById('root'));
+ReactDOM.render(<FruitFilter fruits={fruitList} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+var fruitList = [
+    'Acai', 
+    'Aceola',
+    'Apple',
+    'Apricots',
+    'Avocado',
+    'Banana',
+    'Blackberry',
+    'Blueberries',
+    'Camu Camu berry',
+    'Cherries',
+    'Coconut',
+    'Cranberry',
+    'Cucumber',
+    'Currents',
+    'Dates',
+    'Durian',
+    'Fig',
+    'Goji berries',
+    'Gooseberry',
+    'Grapefruit',
+    'Grapes',
+    'Jackfruit',
+    'Kiwi',
+    'Kumquat',
+    'Lemon',
+    'Lime',
+    'Lucuma',
+    'Lychee',
+    'Mango',
+    'Mangosteen',
+    'Melon',
+    'Mulberry',
+    'Nectarine',
+    'Orange',
+    'Papaya',
+    'Passion Fruit',
+    'Peach',
+    'Pear',
+    'Pineapple',
+    'Plum',
+    'Pomegranate',
+    'Pomelo',
+    'Prickly Pear',
+    'Prunes',
+    'Raspberries',
+    'Strawberries',
+    'Tangerine/Clementine',
+    'Watermelon'
+  ]
