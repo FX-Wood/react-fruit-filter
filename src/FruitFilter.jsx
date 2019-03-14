4d64298601a335c94a935fdac349d6e3acb3d1d1
FX-Wood <fxwood2@gmail.com>
2019-03-14 10:44:13 -0700

    Adding constructor and state

--- src/FruitFilter.jsx
+++ src/FruitFilter.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './FruitFilter.css';
+import FruitFilterer from './FruitFilterer'
+import FruitList from './FruitList'
 
 class FruitFilter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fruitArray: props.fruits,
+      filterValue: ''
+    }
+  }
   render() {
     return (
       <div className="FruitFilter">
+        <FruitFilterer filter={this.state.filterValue} />
+        <FruitList fruits={this.state.fruitsToDisplay} />
       </div>
     );
   }
