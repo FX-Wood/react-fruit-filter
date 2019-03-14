8cf0877d591fbf474153be92143ba7a1a5b87e1f
FX-Wood <fxwood2@gmail.com>
2019-03-14 10:34:14 -0700

    Adding a filterer component

---
+++ src/FruitFilterer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FruitFilterer extends Component {
+    render() {
+        return (
+            <ul>
+                <input type="text"/>
+            </ul>
+        )
+    }
+}
+
+export default FruitFilerer
