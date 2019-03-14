598e6031a400aae72cfb1683f9737b18e0cf8a4f
FX-Wood <fxwood2@gmail.com>
2019-03-14 10:24:51 -0700

    Refreshing boilerplate

---
+++ src/FruitFilter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './FruitFilter.css';
+
+class FruitFilter extends Component {
+  render() {
+    return (
+      <div className="FruitFilter">
+      </div>
+    );
+  }
+}
+
+export default FruitFilter;
--- src/FruitFilter.test.jsx
+++ src/FruitFilter.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import FruitFilter from './FruitFilter';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<FruitFilter />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
