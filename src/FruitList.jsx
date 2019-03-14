d6ab0387d4008e19a63045686033835c31afc918
FX-Wood <fxwood2@gmail.com>
2019-03-14 10:43:21 -0700

    Adding fruit list component

---
+++ src/FruitList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FruitList extends Component {
+    render() {
+        return (
+            <li> I'm a banana</li>
+        )
+    }
+}
+
+export default FruitList;
